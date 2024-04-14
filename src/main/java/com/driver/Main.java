d6e6893bd07ba40c02ab8e900e177f5650461cb0
Roshni <roshni81198singh@gmail.com>
2024-04-15 00:09:05 +0530

    Encapsulation implemented

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        //rw.name = "Roshni";
+        rw.setName("Roshni");
+        String name = rw.getName();
+    }
 }
